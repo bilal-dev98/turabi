c320f65a330591bae88753908c27079f25c632a6
bilal-dev98 <mrbilalgfx@gmail.com>
2026-08-12 00:32:56 +0500

    feat: Install @vercel/speed-insights and integrate <SpeedInsights /> component in root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter, Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import StoreProvider from "@/app/StoreProvider";
 import InitData from "@/components/InitData";
 import "./globals.css";
@@ -143,6 +144,7 @@ export default function RootLayout({ children }) {
                     {children}
                 </StoreProvider>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
